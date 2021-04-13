69b4ffb836a924c4fde35323ff71860d4392a119
Mahdi Aslami <mahdyaslami@gmail.com>
2021-04-13 19:37:02 +0430

    Cover hasKey and getKey.

--- tests/KeyValueValidatorTest.php
+++ tests/KeyValueValidatorTest.php
@@ -4,7 +4,7 @@ namespace Tests\Validation;
 
 use PHPUnit\Framework\TestCase;
 use Simplex\Validation\Contracts\KeyValueValidator;
-use Simplex\Validation\Exceptions\KeyNotFoundException;
+use stdClass;
 
 final class KeyValueValidatorTest extends TestCase
 {
@@ -38,7 +38,7 @@ final class KeyValueValidatorTest extends TestCase
         $catched = false;
         try {
             $rule->validate(0);
-        } catch (KeyNotFoundException $e) {
+        } catch (\Exception $e) {
             $this->assertEquals('Error.', $e->getMessage());
             $catched = true;
         }
@@ -70,6 +70,12 @@ final class KeyValueValidatorTest extends TestCase
      */
     public function check_key_exists_with_std_object()
     {
+        $rule = new TestKeyValueRule('id');
+
+        $object = new stdClass();
+        $object->id = null;
+
+        $this->assertTrue($rule->hasKey($object), 'Key should exists in std object.');
     }
 
     /**
@@ -78,6 +84,13 @@ final class KeyValueValidatorTest extends TestCase
      */
     public function check_key_exists_with_associative_array()
     {
+        $rule = new TestKeyValueRule('id');
+
+        $array = [
+            'id' => null
+        ];
+
+        $this->assertTrue($rule->hasKey($array), 'Key should exists in associative array.');
     }
 
     /**
@@ -86,6 +99,12 @@ final class KeyValueValidatorTest extends TestCase
      */
     public function get_key_value_with_std_object()
     {
+        $rule = new TestKeyValueRule('id');
+
+        $object = new stdClass();
+        $object->id = 'value';
+
+        $this->assertEquals('value', $rule->getKey($object), 'Value is incorrect in std object.');
     }
 
     /**
@@ -94,6 +113,13 @@ final class KeyValueValidatorTest extends TestCase
      */
     public function get_key_value_with_associative_array()
     {
+        $rule = new TestKeyValueRule('id');
+
+        $array = [
+            'id' => 'value'
+        ];
+
+        $this->assertEquals('value', $rule->getKey($array), 'Value is incorrect in associative array.');
     }
 }
 
@@ -102,5 +128,15 @@ class TestKeyValueRule extends KeyValueValidator
     public function validate($value)
     {
         $this->validateChildren($value);
+    }
+
+    public function hasKey($value)
+    {
+        return parent::hasKey($value);
+    }
+
+    public function getKey($value)
+    {
+        return parent::getKey($value);
     }
 }
