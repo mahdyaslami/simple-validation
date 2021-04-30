1cdb0022065dc888b80aad903b365dcc84d48819
Mahdi Aslami <mahdyaslami@gmail.com>
2021-04-30 18:10:28 +0430

    Add doc to contracts.

--- src/Contracts/CompositeValidator.php
+++ src/Contracts/CompositeValidator.php
@@ -6,6 +6,12 @@ abstract class CompositeValidator implements ValidatorInterface
 {
     private $children = [];
 
+    /**
+     * Add array of children to children list.
+     * 
+     * @param array<ValidatorInterface> $children
+     * @return void
+     */
     public function addMany(array $children)
     {
         foreach ($children as $child) {
@@ -13,11 +19,23 @@ abstract class CompositeValidator implements ValidatorInterface
         }
     }
 
+    /**
+     * Add a ValidatorInterface child to children list.
+     * 
+     * @param ValidatorInterface $child
+     * @return void
+     */
     public function add(ValidatorInterface $child)
     {
         array_push($this->children, $child);
     }
 
+    /**
+     * Call validate method of all children with appropriate value.
+     * 
+     * @param mixed $value
+     * @return void
+     */
     protected function validateChildren($value)
     {
         foreach ($this->children as $child) {
--- src/Contracts/KeyValueValidator.php
+++ src/Contracts/KeyValueValidator.php
@@ -8,20 +8,44 @@ abstract class KeyValueValidator extends CompositeValidator
 {
     protected $key;
 
-    public function __construct($key, array $rules = [])
+    /**
+     * Create instance of KeyValudValidator.
+     * 
+     * Its necessary to know what key you want to validate.
+     * and what is your rule for its value.
+     * 
+     * @param string $key
+     */
+    public function __construct(string $key, array $rules = [])
     {
         $this->key = $key;
 
         $this->addMany($rules);
     }
 
-    protected function hasKey($value)
+    /**
+     * Check if key exists.
+     * 
+     * work for both associative array or stdObject.
+     * 
+     * @param array|object $value
+     * @return bool
+     */
+    protected function hasKey($value): bool
     {
         return is_object($value) ?
             property_exists($value, $this->key)
             : array_key_exists($this->key, $value);
     }
 
+    /**
+     * Get value of existing key.
+     * 
+     * work for both associative array or stdObject.
+     * 
+     * @param array|object $value
+     * @return mixed
+     */
     protected function getKey($value)
     {
         return is_object($value) ?
